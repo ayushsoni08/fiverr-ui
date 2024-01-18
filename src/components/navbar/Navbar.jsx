09892a1ea289f4248c4f6c9928f6b85d53d1b3ec
Ayush Soni <ayushsoni0718@gmail.com>
2024-01-18 22:58:43 +0530

    second commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,74 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Navbar.scss';
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="logo">
-                <span className="text">fiverr</span>
-                <span className="dot">.</span>
+
+    const[active, setActive] = useState(false);
+    const[open, setOpen] = useState(false);
+
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false);
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', isActive);
+
+        return () => {
+            window.removeEventListener('scroll', isActive);
+        }
+    }, [])
+
+    const currentUser = {
+        id: 1,
+        username: "Chaynika",
+        isSeller: true,
+    }
+
+    return (
+        <div className={active ? "navbar active" : "navbar"}>
+            <div className="container">
+                <div className="logo">
+                    <span className="text">fiverr</span>
+                    <span className="dot">.</span>
+                </div>
+                <div className="links">
+                    <span>Fiverr Business</span>
+                    <span>Explore</span>
+                    <span>English</span>
+                    <span>Sign in</span>
+                    {!currentUser?.isSeller && <span>Become a Seller</span>}
+                    {!currentUser &&  <button>Join</button>}
+                    {currentUser && (
+                        <div className="user" onClick={()=>setOpen(!open)}>
+                            <img 
+                                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+                                alt="" 
+                            />
+                            <span>{currentUser?.username}</span>
+                            {open && <div className="options">
+                                {currentUser?.isSeller && (
+                                    <>
+                                        <span>Gigs</span>
+                                        <span>Add New Gig</span>
+                                    </>
+                                )}
+                                <span>Orders</span>
+                                <span>Messages</span>
+                                <span>Logout</span>
+                            </div>}
+                        </div>
+                    )}
+                </div>
             </div>
-            <div className="links">
-                <span>Fiverr Business</span>
-                <span>Explore</span>
-                <span>English</span>
-                <span>Sign in</span>
-                <span>Become a Seller</span>
-                <button>Join</button>
-            </div>
+            {active && (
+                <>
+                    <hr />
+                    <div className="menu">
+                        <span>Test1</span>
+                        <span>Test2</span>
+                    </div>
+                </>
+            )}
         </div>
-        <hr />
-        <div className="menu">
-            <span>Test1</span>
-            <span>Test2</span>
-        </div>
-    </div>
   )
 }
 
